33585ee380506eed0909f6ac58027d3363acaa35
kawaldeep18 <chhabrasofia@gmail.com>
2020-02-08 12:05:28 -0500

    add files

---
+++ app/src/main/java/com/example/newyork/Main2Activity.java
@@ -0,0 +1,16 @@
+package com.example.newyork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        
+    }
+}
---
+++ app/src/main/java/com/example/newyork/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.newyork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText edt_logemail=findViewById(R.id.edt_logemail);
+        final EditText edt_logpassword=findViewById(R.id.edt_logpassword1);
+        final Button logu=findViewById(R.id.logu);
+        final Intent intent1 =new Intent(this,Main2Activity.class);
+        //button
+        logu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+if (!edt_logemail.getText().toString().equals("admin")){
+    if (!edt_logemail.getText().toString().equals("username")|| !edt_logpassword.getText().toString().equals("password")) {
+        if (!edt_logemail.getText().toString().equals("username")) {
+            edt_logemail.setError("Username is incorrect");
+        }
+        if (!edt_logpassword.getText().toString().equals("password")) {
+            edt_logpassword.setError("Password is incorrect");
+        }
+    } else {
+        startActivity(intent1);
+    }
+    }
+}
+            })
+        ;
+    }
+}
