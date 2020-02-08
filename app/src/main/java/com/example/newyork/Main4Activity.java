e1bc0b76b90adc0c9a9d8c392ef1cc6c34cd5746
sandeep randhawa <sandeepsingh13>
2020-02-08 13:07:47 -0800

    third coomit by sandeep

--- app/src/main/java/com/example/newyork/Main4Activity.java
+++ app/src/main/java/com/example/newyork/Main4Activity.java
@@ -10,5 +10,11 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+final Button press=findViewById(R.id.back);
+        final Intent intent4=new Intent(this,Main3Activity.class);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent4);
     }
 }
